bf9fbecb0ff567975f3fd249c6eb968810fe2329
Drew <drewbuschhorn@gmail.com>
2023-04-29 21:11:37 -0400

    Refactored for generic use.

--- my_parse.py
+++ my_parse.py
@@ -1,34 +1,59 @@
+# Taken from: https://github.com/pyannote/pyannote-audio https://huggingface.co/pyannote/speaker-diarization
 # 1. visit hf.co/pyannote/speaker-diarization and hf.co/pyannote/segmentation and accept user conditions (only if requested)
 # 2. visit hf.co/settings/tokens to create an access token (only if you had to go through 1.)
 # 3. instantiate pretrained speaker diarization pipeline
+
+# IMPORTANT! APPLY https://github.com/pyannote/pyannote-audio/pull/1326/files
+# if not already merged
+
+import argparse
 import csv
+import os
+from dotenv import load_dotenv
 from pyannote.audio import Pipeline
 from pydub import AudioSegment
-pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization",
-                                                                        use_auth_token="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")
-                                                                        
-
-# 4. apply pretrained pipeline
-pipeline = pipeline.to(0)
-diarization = pipeline("input/episode-10.mp3", num_speakers=2)
-audio_file = AudioSegment.from_file("input/episode-10.mp3")
 
 
-# 5. print the result
-audio_snippets = []
-for turn, _, speaker in diarization.itertracks(yield_label=True):
-    start_ms = int(turn.start * 1000)
-    end_ms = int(turn.end * 1000)
-    duration = end_ms - start_ms
-    filename = f"output/{start_ms}ms-{end_ms}ms_speaker_{speaker}.mp3"
-    print (filename)
-    audio_snippet = audio_file[start_ms:end_ms]
-    audio_snippet.export(filename, format="mp3")
-    audio_snippets.append((duration, speaker, start_ms, end_ms))
+def do_diarization(filename, hf_token, num_speakers = None):
+    pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization", use_auth_token=hf_token)
+                                                                            
+
+    # 4. apply pretrained pipeline
+    pipeline = pipeline.to(0) # Set to use first found CUDA GPU
+    diarization = pipeline(filename, num_speakers=num_speakers)
+    audio_file = AudioSegment.from_file(filename)
 
 
-with open("output/durations_and_speakers.csv", "w", newline="") as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(["Duration (s)", "Speaker", "start_ms", "end_ms"])
-    for duration, speaker, start_ms, end_ms in audio_snippets:
-        writer.writerow([duration, speaker, start_ms, end_ms])
+    # 5. print the result
+    audio_snippets = []
+    for turn, _, speaker in diarization.itertracks(yield_label=True):
+        start_ms = int(turn.start * 1000)
+        end_ms = int(turn.end * 1000)
+        duration = end_ms - start_ms
+        filename = f"output/{start_ms}ms-{end_ms}ms_speaker_{speaker}.mp3"
+        audio_snippet = audio_file[start_ms:end_ms]
+        audio_snippet.export(filename, format="mp3")
+        audio_snippets.append((duration, speaker, start_ms, end_ms))
+
+
+    with open("output/durations_and_speakers.csv", "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Duration (s)", "Speaker", "start_ms", "end_ms"])
+        for duration, speaker, start_ms, end_ms in audio_snippets:
+            writer.writerow([duration, speaker, start_ms, end_ms])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+                    prog='PodcastDiarization',
+                    description='Parses an MP3 file into separate speaker chunks'+
+                                ' by diarization. Generates a lot of of files in output/',
+                    epilog='Run my_transcript.py after completed')
+    parser.add_argument('--file', metavar='-f', nargs=1, required=True,
+                    help='input file to process')
+    parser.add_argument('--num_speakers', metavar='-s', type=int, default=None, required = False,
+                    help='best guess on number of speakers in file, or don\'t specify')
+    args = parser.parse_args()
+    load_dotenv()
+
+    do_diarization(args.file, os.getenv('HUGGING_FACE_TOKEN'), args.num_speakers)
--- my_script_builder.py
+++ my_script_builder.py
@@ -1,6 +1,9 @@
+import argparse
 import collections
 from glob import glob
+import json
 import os
+from dotenv import load_dotenv
 from parse import parse
 import pysrt
 from thefuzz import fuzz
@@ -14,7 +17,6 @@ def convert_ms_to_hms(milliseconds):
 
 def write_line(file, queue, line):
   if is_a_repeat(queue, line):
-    print (['repeat', line, queue])
     return
 
   queue.append(line)
@@ -26,116 +28,106 @@ def is_a_repeat(queue, line):
     fuzz_value = fuzz.partial_ratio(line, queue_item)
     step_threshold = (0.99**(index+1))*100
     if  fuzz_value > step_threshold:
-      print (['fuzz', line, queue_item, index, step_threshold,  fuzz_value])
       return True
   return False
 
+def create_script(final_file = 'output/real.txt', speakers = []):
+  files = [os.path.normpath(i) for i in glob('output/*.mp3.srt')]
 
-files = [os.path.normpath(i) for i in glob('output/*.mp3.srt')]
+  results = []
+  for filename in files:
+    result = parse('output\\{start_ms:d}ms-{end_ms:d}ms_speaker_{speaker_id:S}.mp3.srt', filename)
+    speaker_name = result['speaker_id']
+    for speaker in speakers:
+      if result['speaker_id'] == speaker['speaker_id']:
+        speaker_name = speaker['name']
+        break
 
-results = []
-for filename in files:
-  result = parse('output\\{start_ms:d}ms-{end_ms:d}ms_speaker_{speaker_id:S}.mp3.srt', filename)
-  if result['speaker_id'] == "SPEAKER_00":
-    speaker = "DREW"
-  else:
-    speaker = "KRISTIN"
+    results.append({
+      "parses": {
+        'start_ms':  result['start_ms'],
+        'end_ms': result['end_ms'],
+        'speaker_id': speaker_name
+      },
+      "filename": filename
+    })
 
-  results.append({
-    "parses": {
-      'start_ms':  result['start_ms'],
-      'end_ms': result['end_ms'],
-      'speaker_id': speaker
-    },
-    "filename": filename
-  })
+  results.sort(key=lambda x: x['parses']['start_ms'])
 
-results.sort(key=lambda x: x['parses']['start_ms'])
+  # Read an SRT file
+  with open('output/real.txt', 'w') as out:
+    line_record = collections.deque([], maxlen=10)
+    result_iterator = iter(results)
+    for result in result_iterator:
+      file = result
+      info = file['parses']
 
-# Read an SRT file
-with open('output/debug.txt', 'w') as out:
-  for file in results:
-    subs = pysrt.open(file['filename'])
-    out.write(file['parses']['speaker_id'] + ":" 
-              + str(file['parses']['start_ms']) 
-              + "-" 
-              + str(file['parses']['end_ms']) 
-              + "\n")
-    for sub in subs:
-      out.write(
-        str(file['parses']['start_ms'] + sub.start.ordinal) 
-        + ":" 
-        + str(file['parses']['start_ms'] + sub.end.ordinal)
-        + "-"
-        + str(sub.start.ordinal) 
-        + ":" 
-        + str(sub.end.ordinal) 
-        + " " + sub.text + "\n" + str(sub) + "\n")
-    out.write('\n')
+      try:
+        interjection_files = list(filter(lambda x: True if x['parses']['start_ms'] > info['start_ms']+1 
+                                and x['parses']['end_ms'] < info['end_ms'] else False, 
+                                results))
+        interjection_lines = []
+        for j in interjection_files:
+          results.remove(j)
+
+          interjection_line = '\n[interjection] '
+          # out.write(str(convert_ms_to_hms(j['parses']['start_ms'])) + " - ")
+          interjection_line += j['parses']['speaker_id'] + ": "
+          _subs = pysrt.open(j['filename'])
+          for k in _subs:
+            if not is_a_repeat(line_record, k.text):
+              interjection_line += k.text
+              interjection_lines.append({
+                'start_time': j['parses']['start_ms'] + k.start.ordinal,
+                'end_time': j['parses']['end_ms'],
+                'origin': j,
+                'raw_line': k.text,
+                'line': interjection_line
+              })
+      except IndexError:
+        pass
+      except Exception as e:
+        raise e
+
+      subs = pysrt.open(file['filename'])
+      show_original_speaker_after_interjection = False
+      first_line = True
+      for line in subs:
+        if len(line.text.strip()) > 1:
+          if first_line and not is_a_repeat(line_record, line.text.strip()):
+            out.write("\n["+str(convert_ms_to_hms(info['start_ms'])) + "] ")
+            out.write(info['speaker_id'] + ":\n")
+            first_line = False
+
+          if show_original_speaker_after_interjection:
+            out.write("\n[interjection] " + info['speaker_id'] + ": ")
+
+          write_line(out, line_record, line.text.strip())
+          show_original_speaker_after_interjection = False
+          for l in interjection_lines:
+            if l['start_time'] >= file['parses']['start_ms'] and l['end_time'] <= file['parses']['end_ms']:
+              if l['start_time'] >= file['parses']['start_ms'] + line.start.ordinal and \
+                    l['end_time'] <= file['parses']['start_ms'] + line.end.ordinal:
+                      interjection_lines.remove(l)
+                      if not is_a_repeat(line_record, l['raw_line']) and len(l['raw_line'].strip()) > 1:
+                        line_record.append(l['raw_line'])
+                        out.write(l['line'])
+                        show_original_speaker_after_interjection = True
+      if len(subs) > 0 and len(subs[0].text.strip()) > 1:
+        out.write('\n')
+      #out.write(str(convert_ms_to_hms(info['end_ms'])))
 
 
-# Read an SRT file
-with open('output/real.txt', 'w') as out:
-  line_record = collections.deque([], maxlen=10)
-  result_iterator = iter(results)
-  for result in result_iterator:
-    file = result
-    info = file['parses']
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+                    prog='PodcastSRTsToScript',
+                    description='Reads SRT files generated by my_transcribe.py'+
+                                ' in output/*.mp3.srt into a single text file for humans.',
+                    epilog='Find in output/real.txt')
+    parser.add_argument('--names', metavar='-n', default="[]", required = False,
+                    help='Names of speakers in json array format: ' + 
+                          '[{\\"name\\":\\"Drew\\",\\"speaker_id\\":\\"SPEAKER_00\\"},{\\"name\\":\\"Kristin\\",\\"speaker_id\\":\\"SPEAKER_01\\"}]' )
+    args = parser.parse_args()
+    load_dotenv()
 
-    try:
-      interjection_files = list(filter(lambda x: True if x['parses']['start_ms'] > info['start_ms']+1 
-                              and x['parses']['end_ms'] < info['end_ms'] else False, 
-                              results))
-      interjection_lines = []
-      for j in interjection_files:
-        results.remove(j)
-
-        interjection_line = '\n[interjection] '
-        # out.write(str(convert_ms_to_hms(j['parses']['start_ms'])) + " - ")
-        interjection_line += j['parses']['speaker_id'] + ": "
-        _subs = pysrt.open(j['filename'])
-        for k in _subs:
-          if not is_a_repeat(line_record, k.text):
-            interjection_line += k.text
-            interjection_lines.append({
-              'start_time': j['parses']['start_ms'] + k.start.ordinal,
-              'end_time': j['parses']['end_ms'],
-              'origin': j,
-              'raw_line': k.text,
-              'line': interjection_line
-            })
-    except IndexError:
-      pass
-    except Exception as e:
-      raise e
-  
-    print ([['interjections'],interjection_lines])
-
-    subs = pysrt.open(file['filename'])
-    show_original_speaker_after_interjection = False
-    first_line = True
-    for line in subs:
-      if len(line.text.strip()) > 1:
-        if first_line and not is_a_repeat(line_record, line.text.strip()):
-          out.write("\n["+str(convert_ms_to_hms(info['start_ms'])) + "] ")
-          out.write(info['speaker_id'] + ":\n")
-          first_line = False
-
-        if show_original_speaker_after_interjection:
-          out.write("\n[interjection] " + info['speaker_id'] + ": ")
-
-        write_line(out, line_record, line.text.strip())
-        show_original_speaker_after_interjection = False
-        for l in interjection_lines:
-          if l['start_time'] >= file['parses']['start_ms'] and l['end_time'] <= file['parses']['end_ms']:
-            if l['start_time'] >= file['parses']['start_ms'] + line.start.ordinal and \
-                  l['end_time'] <= file['parses']['start_ms'] + line.end.ordinal:
-                    interjection_lines.remove(l)
-                    if not is_a_repeat(line_record, l['raw_line']) and len(l['raw_line'].strip()) > 1:
-                      line_record.append(l['raw_line'])
-                      out.write(l['line'])
-                      show_original_speaker_after_interjection = True
-    if len(subs) > 0 and len(subs[0].text.strip()) > 1:
-      out.write('\n')
-    #out.write(str(convert_ms_to_hms(info['end_ms'])))
-    
+    create_script(speakers = json.loads(args.names))
